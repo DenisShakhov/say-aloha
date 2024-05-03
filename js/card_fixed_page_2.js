827c6411beceeb79a73fffe6e24968157cfa7cf0
Your Name <you@example.com>
2024-05-03 16:53:15 +0300

    page1

--- js/card_fixed_page_2.js
+++ js/card_fixed_page_2.js
@@ -6,21 +6,22 @@ document.addEventListener('DOMContentLoaded', () => {
         const lastElementRect = lastElement.getBoundingClientRect();
         const lastElementBottom = window.scrollY + lastElementRect.bottom;
         const cardHeight = card.offsetHeight;
-        const stopPosition = lastElementBottom - cardHeight;
-        const top_px = parseInt(stopPosition - cardHeight - cardHeight + 250)  // Высчитываем позицию остановки так, чтобы низ карточки совпадал с низом последнего элемента
-
+        const stopPosition = lastElementBottom - cardHeight - 100;
+        
         if (window.scrollY >= 1100 && window.scrollY <= stopPosition) {
-            card.classList.add('top-24', 'fixed', 'top-24') ; 
-            card.classList.remove('absolute', `top-[${top_px}px]`)
+            card.classList.add('top-24', 'fixed');
+            card.classList.remove('absolute');
+            card.style.top = null;
         } else if (window.scrollY > stopPosition) {
-            card.classList.add('absolute' , 'static', `top-[${top_px}px]`) ; 
-            // card.classList.add('absolute' , 'static', `top-[${parseInt(stopPosition - cardHeight - cardHeight - 10)}px]`) ; 
-            card.classList.remove('fixed', 'top-24')
+            card.classList.add('absolute');
+            card.style.top = `${stopPosition - cardHeight - 420}px`;
+            card.classList.remove('fixed', 'top-24');
         } else {
-            card.classList.remove('absolute', 'fixed', `top-[${top_px}px]`)
+            card.classList.remove('absolute', 'fixed', 'top-24');
+            card.style.top = null;
         }
     }
 
     window.addEventListener('scroll', updateCardPosition);
-    updateCardPosition(); // Вызываем функцию при загрузке для инициализации
+    updateCardPosition(); // Initialize on load
 });
